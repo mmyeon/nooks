04d686caa3d2075a82ae21483714d1a45cb0e7fe
mmyeon <mindfulyeon@gmail.com>
2020-12-17 00:51:44 +0900

    [useInput]  input 추가,  입력받은 값으로  value state 설정

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React, { useState } from "react";
 
+// 특정 문자 못오게 하는 유효성 검사 추가
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    // console.log(e.target)
+    const {
+      target: { value },
+    } = event;
+  };
+
+  return { value, onChange };
+  //{value:value} {value : initialValue}를 생략한 것
+};
+
 const App = () => {
-  const [item, setItem] = useState(1);
-
-  const incrementItem = () => {
-    setItem(item + 1);
-  };
-
-  const decrementItem = () => {
-    setItem(item - 1);
-  };
+  const name = useInput("Mr.");
+  const email = useInput("@");
 
   return (
     <div className="App">
-      <h1>Hello {item}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button onClick={incrementItem}>+</button>
-      <button onClick={decrementItem}>-</button>
+      <h1>Hello</h1>
+      {/* <input placeholder="Name" value={name.value} /> */}
+      <input placeholder="Name" {...name} />
+      <input placeholder="Email" {...email} />
+      {/* 전개연산자로 name 안에 있는 모든 것을 언패킹해줌 */}
+      {/* 그냥 안에 있는 모든 것을 언패킹해줌 - 전개연산자는 혁명적임 */}
     </div>
   );
 };
