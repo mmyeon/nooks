d9162f74c37e18132fea0ad31e3643ec6b35f695
mmyeon <mindfulyeon@gmail.com>
2020-12-17 00:48:29 +0900

    [useState] useState 활용해서 1씩 증가,감소하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-function App() {
-  return <div className="App">hi</div>;
-}
+import React, { useState } from "react";
+
+const App = () => {
+  const [item, setItem] = useState(1);
+
+  const incrementItem = () => {
+    setItem(item + 1);
+  };
+
+  const decrementItem = () => {
+    setItem(item - 1);
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello {item}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={incrementItem}>+</button>
+      <button onClick={decrementItem}>-</button>
+    </div>
+  );
+};
 
 export default App;
