433c72ed6fcf39ae5ff1deeb55653ba49fee8688
mmyeon <mindfulyeon@gmail.com>
2020-12-30 22:36:52 +0900

    [useNetwork] Online / Offline인지 알려주는  useNetwork 훅 만들기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import useFadeIn from "./useFadeIn";
+import useNetwork from "./useNetwork";
+
+// TODO: 네트워크가 바뀔때마다 함수 호출하기
 
 const App = () => {
-  const fadeInH1 = useFadeIn(1, 2);
-  const fadeInP = useFadeIn(5);
+  const handleNetworkChange = (onLine) =>
+    console.log(console.log(onLine ? "We just went online" : "We are offline"));
+  const onLine = useNetwork(handleNetworkChange);
 
   return (
     <div>
-      <h1 {...fadeInH1}>Hi</h1>
-      <p {...fadeInP}>What's going on ???????????</p>
+      <h1>{onLine ? "Online" : "Offline"}</h1>
     </div>
   );
 };
